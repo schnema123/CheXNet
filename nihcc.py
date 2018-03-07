b43da15810174605ffdc03359a915c266a608812
schnema123 <marcelschneider5@outlook.de>
2018-03-07 23:21:07 +0100

    Some changes

--- nihcc.py
+++ nihcc.py
@@ -1,47 +1,61 @@
 import tensorflow as tf
 import densenet
+import nihcc_dataset
 
-# Our input is a 224x224 (Or 256x256?) 8bit per Channel 1 Channel png
-# => Tensor 224x224 => 
-example_image = tf.random_normal([50176])
 
 def model_fn(
-    features, 
-    labels, 
-    mode, 
-    params):
-     
+        features,
+        labels,
+        mode):
+
     in_training = (mode == tf.estimator.ModeKeys.TRAIN)
-    logits = densenet_121(features, num_classes=15, is_training=in_training)
+    net, end_points = densenet.densenet121(
+        features, num_classes=15, is_training=in_training)
+
+    print(net)
+    print(end_points["predictions"])
+
+    logits = end_points["predictions"]
+    logits = tf.reshape(logits, [-1, 15])
+
+    print(logits)
 
     predictions = {
-        "classes": tf.argmax(input = logits, axis=1),
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+        "classes": tf.argmax(input=logits, axis=1),
+        "probabilities": tf.nn.softmax(net, name="softmax_tensor")
     }
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode = mode, predictions=predictions)
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
     # Calculate loss
-    onehot_labels = tf.one_hot(indices = tf.cast(labels, tf.int32), depth=10)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=labels, logits=logits)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
         train_op = optimizer.minimize(
-            loss = loss,
+            loss=loss,
             global_step=tf.train.get_global_step()
         )
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-    
+
     eval_metric_ops = {
         "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])
     }
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-def input_fn():
+
+def input_fn(batch_size):
     """An input function for training"""
-    dataset = tf.data.Dataset.from_tensor(example_image)
-    dataset = dataset.shuffle(1000).repeat()
-    return dataset.make_one_shot_iterator().get_next()
+    ds = nihcc_dataset.create_dataset()
+    ds = ds.shuffle(1000).repeat().batch(batch_size)
+    return ds.make_one_shot_iterator().get_next()
 
 
+def main():
+    estimator = tf.estimator.Estimator(model_fn=model_fn)
+    estimator.train(input_fn=lambda: input_fn(50))
+
+
+if __name__ == "__main__":
+    main()
--- nihcc_dataset.py
+++ nihcc_dataset.py
@@ -1,7 +1,10 @@
+import os
+import csv
+
+
 import tensorflow as tf
 
 DATA_PATH = "../nihcc/"
-IMAGE_PATH = DATA_PATH + "image_all/"
 
 TRAIN_DATA = "data_train.csv"
 EVAL_DATA = "data_eval.csv"
@@ -14,29 +17,59 @@ FIELDS_DEFAULT = [[""] * 11]
 
 FINDINGS = ["No Finding", "Atelectasis", "Cardiomegaly", "Effusion", "Infiltration", "Mass", "Nodule", "Pneumonia",
             "Pneumothorax", "Consolidation", "Edema", "Emphysema", "Fibrosis", "Pleural_Thickening", "Hernia"]
+NUM_FINDINGS = len(FINDINGS)
 
-def _parse_tfrecord(tfrecord):
 
-    features = {
-        'image': tf.FixedLenFeature([], tf.String),
-        'findings': tf.FixedLenFeature([15], tf.int64)
-    }
-    record = tf.parse_single_example(tfrecord)
+def _read_image(filename, label):
 
-    images_path = tf.constant("../nihcc/images_scaled/")
-    image_full_path = tf.string_join([images_path, record[image]])
-
-    image_string = tf.read_file(image_full_path)
+    images_path = tf.constant("J:/BA/nihcc/images_scaled/")
+    file_path = tf.string_join([images_path, filename])
+    image_string = tf.read_file(file_path)
     image_decoded = tf.image.decode_image(image_string)
+    image_decoded = tf.image.convert_image_dtype(image_decoded, tf.float32)
+    image_decoded.set_shape((224, 224, 1))
 
-    return image_decoded, record["findings"]
+    return image_decoded, label
+
+
+def _read_csv():
+
+    images = []
+    labels = []
+
+    csv_path = os.path.join(DATA_PATH, "Data_Entry_2017_small.csv")
+    with open(csv_path) as csv_file:
+
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            images.append(row["Image Index"])
+
+            # Make vector of labels
+            findings = row["Finding Labels"].split("|")
+
+            findings_vector = [0 for _ in range(NUM_FINDINGS)]
+            for finding in findings:
+                findings_vector[FINDINGS.index(finding)] = 1
+
+            assert(1 in findings_vector)
+                
+            labels.append(findings_vector)
+
+    return tf.convert_to_tensor(images), tf.convert_to_tensor(labels)
 
 
 def create_dataset():
 
-    tf_record_path = "../nihcc/tfrecords/train.tfrecord"
+    # Get csv data
+    images, labels = _read_csv()
 
-    # Pass this in as a flag
-    ds = tf.data.TFRecordDataset(tf_record_path)
-    ds = ds.map(_parse_tfrecord)
+    print(images)
+    print(labels)
 
+    ds = tf.data.Dataset.from_tensor_slices((images, labels))
+    ds = ds.map(_read_image)
+
+    return ds
+
+
+create_dataset()
