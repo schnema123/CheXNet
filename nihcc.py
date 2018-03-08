0df79b41cb7a9c4ca5296c40781f22d8b8e06d49
schnema123 <marcelschneider5@outlook.de>
2018-03-08 10:21:05 +0100

    Added some logging. Trying out some things

--- nihcc.py
+++ nihcc.py
@@ -8,28 +8,29 @@ def model_fn(
         labels,
         mode):
 
+    tf.summary.image("image", features)
+
     in_training = (mode == tf.estimator.ModeKeys.TRAIN)
     net, end_points = densenet.densenet121(
         features, num_classes=15, is_training=in_training)
 
-    print(net)
-    print(end_points["predictions"])
-
     logits = end_points["predictions"]
     logits = tf.reshape(logits, [-1, 15])
 
-    print(logits)
-
     predictions = {
         "classes": tf.argmax(input=logits, axis=1),
-        "probabilities": tf.nn.softmax(net, name="softmax_tensor")
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
     }
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
+    tensor_labels = tf.identity(labels, "labels")
     # Calculate loss
     loss = tf.losses.softmax_cross_entropy(
-        onehot_labels=labels, logits=logits)
+        onehot_labels=tensor_labels, logits=logits)
+
+    tf.summary.scalar("loss", loss)
+
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
@@ -53,8 +54,14 @@ def input_fn(batch_size):
 
 
 def main():
-    estimator = tf.estimator.Estimator(model_fn=model_fn)
-    estimator.train(input_fn=lambda: input_fn(50))
+
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    tensors_to_log = {"probabilities": "softmax_tensor", "labels": "labels"}
+    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=2)
+
+    estimator = tf.estimator.Estimator(model_fn=model_fn, model_dir="J:/BA/tmp/")
+    estimator.train(input_fn=lambda: input_fn(5), hooks=[logging_hook])
 
 
 if __name__ == "__main__":
