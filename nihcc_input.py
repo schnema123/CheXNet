8632b1334915c937b772e08a12bbd3336716d7aa
schnema123 <marcelschneider5@outlook.de>
2018-04-12 09:06:46 +0200

    Added input for training

--- nihcc_input.py
+++ nihcc_input.py
@@ -11,7 +11,7 @@ FINDINGS = ["Atelectasis", "Cardiomegaly", "Effusion", "Infiltration", "Mass", "
 NUM_FINDINGS = len(FINDINGS)
 
 
-def _read_image(filepath, label):
+def _read_image(filepath, label, is_training):
 
     image_string = tf.read_file(filepath)
 
@@ -23,20 +23,23 @@ def _read_image(filepath, label):
     # TODO: It does seem that tensorflow acts weird on resizing images
     # https://hackernoon.com/how-tensorflows-tf-image-resize-stole-60-days-of-my-life-aba5eb093f35
 
-    image_size = [256, 256]
-    image_decoded = tf.image.resize_images(image_decoded, image_size)
-    
-    crop_size = [224, 224, 3]
-    image_decoded = tf.random_crop(image_decoded, crop_size)
+    if is_training:
+        image_size = [256, 256]
+        image_decoded = tf.image.resize_images(image_decoded, image_size)
+
+        crop_size = [224, 224, 3]
+        image_decoded = tf.random_crop(image_decoded, crop_size)
+
+        image_decoded = tf.image.random_flip_left_right(image_decoded)
+
+    else:
+        image_decoded = tf.image.resize_images(image_decoded, [224, 224])
 
     image_decoded = tf.image.per_image_standardization(image_decoded)
-    image_decoded = tf.image.random_flip_left_right(image_decoded)
-
-    # image_decoded = tf.reshape(image, [-1, 224, 224])
-    # image_decoded = tf.stack([image, image, image], axis=3)
     image_decoded.set_shape([224, 224, 3])
 
     return image_decoded, label
+
 
 def _read_txt(filename):
 
@@ -65,7 +68,7 @@ def _read_txt(filename):
                 for idx, item in enumerate(imageLabel):
                     if item == 1:
                         finding = FINDINGS[idx]
-                        num_finding[finding] += 1 
+                        num_finding[finding] += 1
 
                 images.append(imagePath)
                 labels.append(imageLabel)
@@ -90,15 +93,18 @@ def create_dataset(mode):
         filename = "../dataset/test_1.txt"
 
     # Reads image file names and labels
-    images, labels = _read_txt(filename)   
+    images, labels = _read_txt(filename)
 
     images = tf.convert_to_tensor(images)
-    labels= tf.convert_to_tensor(labels)
+    labels = tf.convert_to_tensor(labels)
 
     ds = tf.contrib.data.Dataset.from_tensor_slices((images, labels))
 
     ds = ds.shuffle(100000)
-    ds = ds.map(_read_image, num_threads=20, output_buffer_size=50)
+
+    in_training = (mode == tf.estimator.ModeKeys.TRAIN)
+    ds = ds.map(lambda filepath, label: _read_image(filepath, label, in_training),
+                num_threads=20, output_buffer_size=50)
     ds = ds.batch(16)
 
     return ds
