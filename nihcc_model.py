e3f47f015bfe57cc4de0cf31b5308f8a67fca8a2
schnema123 <marcelschneider5@outlook.de>
2018-04-12 09:07:13 +0200

    Removed multi gpu training

--- nihcc_model.py
+++ nihcc_model.py
@@ -56,48 +56,26 @@ def model_fn(
             loss = loss_fn(labels, logits)
             return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-    # Setup multi-gpu training
-    num_gpu = 2
+    tf.summary.image("image", image, max_outputs=16)
+    logits = network_fn(image, True)
+
+    loss = loss_fn(labels, logits)
+
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    total_loss = loss # + tf.add_n(reg_losses)
+    tf.summary.scalar("total_loss", total_loss)
 
     global_step = tf.train.get_global_step()
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999)
 
-    image_split = tf.split(image, num_gpu)
-    labels_split = tf.split(labels, num_gpu)
-
-    tower_gradients = []
-
-    with tf.variable_scope(tf.get_variable_scope()):
-        for x in range(num_gpu):
-            with tf.device("/gpu:{}".format(x)):
-                with tf.name_scope("classification_{}".format(x)) as scope:
-
-                    tf.summary.image("image", image_split[x], max_outputs=16)
-                    logits = network_fn(image_split[x], True)
-
-                    loss = loss_fn(labels_split[x], logits)
-
-                    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope)
-                    with tf.control_dependencies(update_ops):
-
-                        losses = tf.get_collection(tf.GraphKeys.LOSSES, scope)
-                        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-
-                        total_loss = tf.add_n(losses + reg_losses, name="total_loss")
-
-                        tf.summary.scalar("total_loss", total_loss)
-
-                    tf.get_variable_scope().reuse_variables()
-                    gradients = optimizer.compute_gradients(total_loss)
-                    tower_gradients.append(gradients)
-
-    gradients = nihcc_utils.average_gradients(tower_gradients)
-    apply_gradients_op = optimizer.apply_gradients(gradients, global_step=global_step)
-
-    variable_averages = tf.train.ExponentialMovingAverage(0.9999, global_step)
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        minimize_op = optimizer.minimize(total_loss, global_step=global_step)
+        
+    variable_averages = tf.train.ExponentialMovingAverage(0.9999, num_updates=global_step)
     variable_averages_op = variable_averages.apply(tf.trainable_variables())
     
-    train_op = tf.group(apply_gradients_op, variable_averages_op)
+    train_op = tf.group(minimize_op, variable_averages_op)
     return tf.estimator.EstimatorSpec(mode=mode, loss=total_loss, train_op=train_op)
 
     
